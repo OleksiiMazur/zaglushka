6e831994dc68892ed99006f4c429741bed4e8451
OleksiiMazur <oleksiimazur111@gmail.com>
2020-05-07 23:12:41 +0300

    large tab in process

--- js/hello-page.js
+++ js/hello-page.js
@@ -1,7 +1,30 @@
 "use strict";
-$(document).ready(function () {
-    let contentCenter = $('.content-center');
-    if ($('.hello-page')) {
-        contentCenter.addClass('hello-content');
+
+function subheaderHtml () {
+    let desktopL = $(window).width() > 1700;
+    let desktopM = $(window).width() <= 1700 && $(window).width() > 1400;
+    let laptop = $(window).width() <= 1400 && $(window).width() > 1024;
+    let tab = $(window).width() <= 1024 && $(window).width() > 768;
+    let mobile = $(window).width() <= 768;
+
+    if (desktopL) {
+
+    } else if (desktopM) {
+
     }
+}
+
+function bannerMenuToggle () {
+    let bannerMenu = $('.banner-menu');
+    let bannerMenuBtn = $('.banner-menu__btn');
+
+    bannerMenuBtn.on('click', function () {
+        bannerMenu.toggleClass('opened');
+    });
+}
+
+
+$(document).ready(function () {
+    bannerMenuToggle();
+    // $(window).on('resize', subheaderHtml);
 });
